255a6778bc5112c798646123a05762c52eccb2af
SANG-SANG <66198932+pktony@users.noreply.github.com>
2022-10-17 21:44:55 +0900

    [홈 화면 UI] 리더보드 완료
     - UI Manager로 리팩토링 필요

--- Assets/__________Scripts/UI/Home/HomeButtons.cs
+++ Assets/__________Scripts/UI/Home/HomeButtons.cs
@@ -7,24 +7,28 @@ public class HomeButtons : MonoBehaviour
 {
     Image playImage;
     Image settingImage;
+    Image leaderBoardImage;
 
     private void Awake()
     {
         Transform buttonParent = transform.GetChild(1);
         playImage = buttonParent.GetChild(0).GetComponent<Image>();
         settingImage = buttonParent.GetChild(1).GetComponent<Image>();
+        leaderBoardImage = buttonParent.GetChild(2).GetComponent<Image>();
     }
 
     public void HideButtons()
     {
         TurnoffButton(playImage);
         TurnoffButton(settingImage);
+        TurnoffButton(leaderBoardImage);
     }
 
     public void ShowButtons()
     {
         TurnOnButton(playImage);
         TurnOnButton(settingImage);
+        TurnOnButton(leaderBoardImage);
     }
 
     private void TurnoffButton(Image image)
